7bcc22d417bfdb7059240c6808eb042cc80666c1
Oleg Apostol <apostol@yandex-team.ru>
2014-11-20 00:14:32 +0300

    init

---
+++ client.js
@@ -0,0 +1,15 @@
+$(function() {
+    app = new window.modules.app();
+
+    // инициаизация
+    app.init($('.app'), [45.116751, 34.158592], 9);
+
+    app.domElem.on('init', function() {
+        app._addPlacemark([45.116751, 34.158592], 'Крым', 'Привет NAMASTE ;)');
+        app._addPlacemark([45.122584, 34.056969], '1', 'пыщь!');
+        app._addPlacemark([45.060327, 33.919640], '2', 'добра тебе ^_^');
+        app._addPlacemark([44.659871, 33.991051], '3', 'однажды один хороший человек...');
+    });
+
+
+});
